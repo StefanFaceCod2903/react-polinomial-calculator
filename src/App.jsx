3598d8b905592921cf357e6951ad06e16898679d
Ștefan Cadar <65918883+StefanFaceCod2903@users.noreply.github.com>
2023-04-22 14:06:55 +0300

    implement all other operations

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const [p1, setP1] = useState('');
   const [p2, setP2] = useState('');
-  const [err, setErr] = useState(false);
+  const [err, setErr] = useState('');
   const [result, setResult] = useState('');
 
   class Polynomial {
@@ -15,7 +15,7 @@ function App() {
     setMap(input) {
       const re = new RegExp("((?<sign>^|[+-])(((?<ac1>([0-9]*[.])?[0-9]+)\\*)?x(\\^(?<rank>[0-9]+))?|(?<ac2>([0-9]*[.])?[0-9]+)))+$");
       if(!input.match(re)) {
-        setErr(true);
+        setErr("Try again! Please follow the example.");
         return;
       }
       const re2 = new RegExp("((?<sign>^|[+-])(((?<ac1>([0-9]*[.])?[0-9]+)\\*)?x(\\^(?<rank>[0-9]+))?|(?<ac2>([0-9]*[.])?[0-9]+)))",'g');
@@ -41,32 +41,32 @@ function App() {
       }
       var rez = '';
       for(var [r, coef] of this.monomials) {
-        const c = coef % 1 === 0 ? coef > 0 ? "+" + coef : "" + coef : coef > 0 ? "+" + coef : "" + coef;
+        const c = coef % 1 === 0 ? coef > 0 ? "+" + coef : "" + coef : coef > 0 ? "+" + coef.toFixed(2) : "" + coef.toFixed(2);
         if(r === 0) {
           rez = rez + c;
         } else if(r===1) {
           if(c==='+1') {
             if(rez.length === 0) {
-              rez = 'x' + rez;
+              rez = rez + 'x';
             } else {
-              rez = '+x' + rez;
+              rez = rez + '+x';
             }
           } else if(c==='-1') {
-              rez = '-x' + rez;
+              rez = rez + '-x';
           } else {
-              rez = c + '*x' + rez;
+              rez = rez + c + '*x';
           }
         } else {
           if(c==='+1') {
             if(rez.length === 0) {
-              rez = 'x^' + r + rez;
+              rez = rez + 'x^' + r;
             } else {
-              rez = '+x^' + r + rez;
+              rez = rez + '+x^' + r;
             }
           } else if (c==='-1'){
-            rez = '-x^' + r + rez;
+            rez = rez + '-x^' + r;
           } else {
-            rez = c + '*x^' + r + rez;
+            rez = rez + c + '*x^' + r;
           }
         }
       }
@@ -95,15 +95,98 @@ function App() {
     return sum;
     
   }
-  
+
+  function difference(p1, p2) {
+    let diff = new Polynomial();
+    for (const key of p1.monomials.keys()) {
+      diff.monomials.set(key, p1.monomials.get(key));
+    }
+    for (const key of p2.monomials.keys()) {
+        if(Array.from(diff.monomials.keys()).some((element) => element==key)) {
+          diff.monomials.set(key, diff.monomials.get(key)-p2.monomials.get(key));
+        } else {
+          diff.monomials.set(key, -p2.monomials.get(key));
+        }
+        if(diff.monomials.get(key) == 0) {
+          diff.monomials.delete(key);
+        }
+    }
+    return diff;
+  }
+
+  function multiply(p1,p2) {
+    const result = new Polynomial();
+        for (const key1 of p1.monomials.keys()) {
+            for (const key2 of p2.monomials.keys()) {
+                if(result.monomials.get(key1+key2)!==undefined) {
+                    result.monomials.set(key2 + key1, result.monomials.get(key1 + key2) + p1.monomials.get(key1) * p2.monomials.get(key2));
+                } else {
+                    result.monomials.set(key2 + key1, p1.monomials.get(key1) * p2.monomials.get(key2));
+                }
+            }
+        }
+        return result;
+  }
+
+  function devide(p1, p2) {
+    var p1Deg = Math.max(...p1.monomials.keys());
+    var p2Deg = Math.max(...p2.monomials.keys());
+    if((p2Deg===0 && p2.get(p2Deg) === 0) || p2.monomials.size === 0) {
+      setErr("Please don't divide by zero.");
+      return [null, null];
+    }
+    var res = new Polynomial();
+    while(p1.monomials.size > 0 && p1Deg >= p2Deg){
+        const toSub = new Polynomial();
+        const k = p1Deg - p2Deg;
+        const c = p1.monomials.get(p1Deg) / p2.monomials.get(p2Deg) ;
+        toSub.monomials.set(k,c);
+        res.monomials.set(k,c);
+        const p3 = multiply(p2, toSub);
+        p1 = difference(p1, p3);
+        p1Deg = Math.max(...p1.monomials.keys());
+        p2Deg = Math.max(...p2.monomials.keys());
+    }
+    return [res, p1];
+  }
+
+  function derivate(p1) {
+    const res = new Polynomial();
+        for (const key of p1.monomials.keys()) {
+            if(key>0) {
+                res.monomials.set(key - 1, p1.monomials.get(key)*key);
+            }
+        }
+        return res;
+  }
+
+  function integrate(p) {
+    if(p.monomials.size === 0) {
+      return new Polynomial();
+    }
+    const res = new Polynomial();
+    for (const key of p.monomials.keys()) {
+      res.monomials.set(key+1, p.monomials.get(key)/(key+1));
+    }
+    return res;
+  }
 
   function Result() {
-    if(err) return <>
+    if(err!='') return <>
     <div class="alert alert-danger" role="alert">
       <h4 class="alert-heading text-left">Error.</h4>
       <p>Something went wrong.</p>
       <hr/>
-      <p class="mb-0">Try again! Please follow the example.</p>
+      <p class="mb-0">{err}</p>
+    </div>
+    </>
+    if(Array.isArray(result)) return <>
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading text-left">Result</h4>
+      <p>{result[0]}</p>
+      <p>{result[1]}</p>
+      <hr/>
+      <p class="mb-0">Do another calculation!</p>
     </div>
     </>
     if(result!=='') return <>
@@ -116,23 +199,49 @@ function App() {
     </>
   }
 
-  const handleSubmit = (e) => {
+  const handleClick = (e, op) => {
     e.preventDefault();
-    setErr(false);
+    setErr('');
     const poli1 = new Polynomial();
     poli1.setMap(p1);
     const poli2 = new Polynomial();
     poli2.setMap(p2);
-    const res = add(poli1, poli2);
-    setResult(res.display());
+    switch(op) {
+      case 0:
+        setResult(add(poli1, poli2).display());
+        break;
+      case 1:
+        setResult(difference(poli1, poli2).display());
+        break;
+      case 2:
+        setResult(multiply(poli1, poli2).display());
+        break;
+      case 3:
+        const [quotient, remainder] = devide(poli1, poli2);
+        if(quotient!=null && remainder!=null) setResult([quotient.display(), "R: " + remainder.display()]);
+        break;
+      case 4:
+        const [d1, d2] = [derivate(poli1), derivate(poli2)];
+        setResult([d1.display(), d2.display()]);
+        break;
+      case 5:
+        const [i1, i2] = [integrate(poli1), integrate(poli2)];
+        setResult([i1.display() + "+c", i2.display() + "+c"]);
+        break;
+      default:
+        // code block
+    } 
   }
   return (
     <>
-    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
+    <div className='svg-container'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
       <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
       <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
     </svg>
-  <form class="mt-3" onSubmit={handleSubmit}>
+    </div>
+  <form>
+  <div class="form-group">
     <div class="mb-3">
       <label class="form-label">First polynomial</label>
       <input type="text" onChange={(e) => setP1(e.target.value)} class="form-control"/>
@@ -143,15 +252,17 @@ function App() {
       <input type="text" onChange={(e) => setP2(e.target.value)} class="form-control" aria-describedby="polinomialExample"/>
       <div id="polinomialExample" class="form-text">Input example: x^2+3*x-6</div>
     </div>
-    <Result/>
-    <div class="btn-group" role="group" aria-label="Basic outlined example">
-      <button type='submit' class="btn btn-outline-dark">Add</button>
-      <button type='submit' class="btn btn-outline-dark">Subtract</button>
-      <button type='submit' class="btn btn-outline-dark">Multiply</button>
-      <button type='submit' class="btn btn-outline-dark">Divide</button>
-      <button type='submit' class="btn btn-outline-dark">Integrate</button>
-      <button type='submit' class="btn btn-outline-dark">Drivate</button>
-    </div>
+  </div>
+  <Result/>
+  <div class="container-fluid">
+    <div class="row gx-1">
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 0)}>Add</button>
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 1)}>Subtract</button>
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 2)}>Multiply</button>
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 3)}>Divide</button>
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 4)}>Drivate</button>
+      <button class="col-6 col-md-4 btn btn-outline-dark btn-block rounded-0" onClick={(e) => handleClick(e, 5)}>Integrate</button></div>
+  </div>
   </form>
   </>);
 }
